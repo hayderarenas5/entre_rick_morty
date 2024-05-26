5810673470c29aca2821fed578e0ce7c6c4613c5
hayderarenas5 <hearenas5@misena.edu.co>
2024-05-26 09:26:29 -0500

    imagen

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     
 
   return (
-    <main className='px-4 min-h-screen bg-[url("/background_page.png")] text-white'>
+    <main className='px-4 min-h-screen bg-[url("/background_page.png")] bg-cover text-white'>
       <Location location={location} setLocation={setLocation}/>
       {/* //?con el ?? quiero que aunque lo de la izquierda sea null envie un array[] 
       //TODO y en ? es si el location es null lo ignore y siga con la puntuacion .residents */} 
